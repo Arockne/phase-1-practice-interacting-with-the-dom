5f5d9ab5cbc0c85221d711dc45c6bbc61fc66a69
Daniel Calvin <arockne2003@yahoo.com>
2021-08-02 11:32:51 -0700

    Implement incrementLikes

--- js/challenge.js
+++ js/challenge.js
@@ -32,6 +32,9 @@ function buttons() {
   
   const plus = document.querySelector('#plus');
   plus.addEventListener('click', incrementCounter);
+
+  const like = document.querySelector('#heart');
+  like.addEventListener('click', likeMessage);
 }
 
 function incrementCounter() {
@@ -43,5 +46,27 @@ function decrementCounter() {
   const counter = document.querySelector('#counter');
   counter.textContent = Number(counter.textContent.trim()) - 1;
 }
+
+
+//somehow I need to associate the amount of likes for a current number
+function likeMessage() {
+  const num = document.querySelector('#counter').textContent.trim();
+  incrementLikes(num);
+  const likeList = document.querySelector('.likes');
+  const li = document.createElement('li');
+  li.textContent = `${num} has been liked ${likeCount[num]} time`;
+  likeList.appendChild(li);
+}
+
+const likeCount = {};
+
+function incrementLikes(num) {
+  if (!likeCount[num]) {
+    likeCount[num] = 1;
+  } else {
+    likeCount[num]++;
+  }
+}
+
 intervalCounter();
 buttons();
